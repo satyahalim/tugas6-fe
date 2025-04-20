7603560080cbff658ca3d168b62d30514bcdcae5
Satyagraha Halim Widiatmojo <160807125+satyahalim@users.noreply.github.com>
2025-04-20 18:34:23 +0700

    Update EditNotes.jsx

--- src/components/EditNotes.jsx
+++ src/components/EditNotes.jsx
@@ -1,6 +1,7 @@
 import React,{useState, useEffect,} from 'react'
 import axios from 'axios'
 import {useNavigate,useParams} from "react-router-dom";
+import { BASE_URL } from '../utils'
 
 const EditNotes = () => {
 const [title,setTitle] = useState("");
