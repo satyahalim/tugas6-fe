7ec16a83ff192477ccfb8b493b65f8f5d8209fc4
Satyagraha Halim Widiatmojo <160807125+satyahalim@users.noreply.github.com>
2025-04-20 18:34:42 +0700

    Update NotesList.js

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import axios from "axios"
 import {Link} from "react-router-dom"
+import { BASE_URL } from '../utils'
 
 const NotesList = () => {
     const [Notes,setNotes] = useState([])
